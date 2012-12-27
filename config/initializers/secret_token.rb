2d19995ba3dc28711354930ea2fb197008b0e368
gabriel-dehan <dehan.gabriel@gmail.com>
2012-12-27 13:47:17 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PageFetcher::Application.config.secret_token = 'd886928b26624f0bf9dd3a1e4401cda996e6def6553aef7e3508d5bc75941005fd8ec64efbdd561faf4f7ed494189386a703fe19d5c515b99f23730b1deb0c97'
