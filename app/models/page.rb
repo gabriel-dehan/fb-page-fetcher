8a16f5e46d1db007edd9808ff42941a027475aa2
gabriel-dehan <dehan.gabriel@gmail.com>
2012-12-27 15:23:32 +0100

    Page model implementation & validation

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,7 @@
 class Page < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :fb_uid, :name, :category, :picture, :likes
+
+  validates :fb_uid,   presence: true
+  validates :name,     presence: true
+  validates :likes,    presence: true,  numericality: true
 end
