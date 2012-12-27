2a2d4583553b37ab47a5bc83de6f9ac2913fb17e
gabriel-dehan <dehan.gabriel@gmail.com>
2012-12-27 17:21:12 +0100

    Pages integration test, action: index

--- test/integration/pages/pages_integration_test.rb
+++ test/integration/pages/pages_integration_test.rb
@@ -1,5 +1,47 @@
 require 'minitest_helper'
 
 describe "Pages Integration Test" do
+  describe 'index' do
+    before do
+      visit root_path
+    end
+    let(:form) { '#new_page' }
 
+    describe 'New page form' do
+      it 'must have a form to add a new page' do
+        page.must have_selector("form#{form}")
+      end
+
+      context 'New page creation' do
+        it 'should create a new page if the form validates' do
+          id = '218044128258628'
+          within(form) do
+            fill_in 'fb_uid', with: id
+            click_button "add_page"
+          end
+          Page.last.fb_uid.must_equal id
+        end # should create a new page
+
+        it 'should display an error if the form invalidates' do
+          within(form) do
+            fill_in 'fb_uid', with: ''
+            click_button "add_page"
+          end
+          page.must have_css('p.error')
+        end # should display error
+
+      end # New page creation
+    end # New page form
+
+    describe 'Page list' do
+      it 'should display a list of all pages' do
+        [Page.first, Page.last].each do |p|
+          page.must have_css('li', text: p.name)
+        end
+      end
+    end # Page list
+  end # index
+
+  describe 'show' do
+  end # show
 end
