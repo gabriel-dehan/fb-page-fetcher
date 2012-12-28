9e4d5a05541ff62dca98a234c887dc335711db45
gabriel-dehan <dehan.gabriel@gmail.com>
2012-12-28 13:41:36 +0100

    Improving facebook::page::build test

--- test/models/facebook_test.rb
+++ test/models/facebook_test.rb
@@ -4,7 +4,7 @@ require 'minitest_helper'
 
 describe Facebook do
   # Mocking the Koala API
-  let(:fb_page) { { name: 'Mock', fb_uid: '218044128258628', category: 'Website', likes: 17 } }
+  let(:fb_page) { { 'name' => 'Mock', 'fb_uid' => '218044128258628', 'category' => 'Website', 'likes' => 17 } }
   before do
     graph = stub(get_object: fb_page)
     Facebook.stubs(:connect).returns(graph)
@@ -22,7 +22,7 @@ describe Facebook do
 
     describe 'Facebook::Page::build' do
       before do
-        @object = Struct.new(:fb_uid, :id, :name, :category, :picture, :likes).new(fb_page[:fb_uid])
+        @object = Struct.new(:fb_uid, :id, :name, :category, :picture, :likes).new(fb_page['fb_uid'])
       end
 
       it 'should raise an exception if a field is missing' do
@@ -35,10 +35,10 @@ describe Facebook do
 
       describe 'returns a filled object' do
         let(:o) { Facebook::Page.build(@object) }
-        its { o.name.must_equal fb_page[:name] }
-        its { o.category.must_equal fb_page[:category] }
-        its { o.picture.must_equal fb_page[:picture] }
-        its { o.likes.must_equal fb_page[:likes] }
+        its { o.name.must_equal fb_page['name'] }
+        its { o.category.must_equal fb_page['category'] }
+        its { o.picture.must_equal fb_page['picture'] }
+        its { o.likes.must_equal fb_page['likes'] }
       end
     end
   end
