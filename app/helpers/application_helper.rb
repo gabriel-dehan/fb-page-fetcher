4c8e52a6a81b222ef002a63288085015465e3068
gabriel-dehan <dehan.gabriel@gmail.com>
2012-12-28 14:05:11 +0100

    ApplicationHelpers#get_title implementation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # Public: generates a title for the current page
+  #
+  # title - A title for the page (default: current action name)
+  #
+  # Returns the full title as a String.
+  def get_title(title = nil)
+    application_name = 'PageFetcher'
+    title = action_name.capitalize if title.nil? && !action_name.nil?
+    (action_name.nil? && title.nil?) ? application_name : "#{ application_name} | #{title}"
+  end
 end
