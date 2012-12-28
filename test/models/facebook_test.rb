8a52001185aa8fa6721c7276da12f42a00eb9430
gabriel-dehan <dehan.gabriel@gmail.com>
2012-12-29 00:53:39 +0100

    Testing Feed::build

--- test/models/facebook_test.rb
+++ test/models/facebook_test.rb
@@ -5,8 +5,40 @@ require 'minitest_helper'
 describe Facebook do
   # Mocking the Koala API
   let(:fb_page) { { 'name' => 'Mock', 'fb_uid' => '218044128258628', 'category' => 'Website', 'likes' => 17 } }
+  let(:fb_feed) { [
+      { 'id'       => '218044128258628',
+        'message'  => 'Foobar foo',
+        'likes'    => { 'count' => 0 },
+        'shared'   => { 'count' => 1 },
+        'picture'  => 'http://foo.bar',
+        'link'     => nil,
+        'created_time' => 'datetime1',
+        'updated_at'   => 'datetime2',
+        'from' => {
+          'id' => '230249320942',
+          'name' => 'Thomas'
+        }
+      },
+      { 'id'       => '218044128258620',
+        'message'  => 'Foobar bar',
+        'likes'    => { 'count' => 18 },
+        'shared'   => nil,
+        'picture'  => nil,
+        'link'     => 'http://bar.foo',
+        'created_time' => 'datetime1',
+        'updated_at'   => 'datetime2',
+        'from' => {
+          'id' => '230249320948',
+          'name' => 'Ian'
+        }
+      }
+    ]
+  }
   before do
-    graph = stub(get_object: fb_page)
+    graph = mock()
+    graph.stubs(:get_object).with(fb_page['fb_uid']).returns(fb_page)
+    graph.stubs(:get_connections).returns(fb_feed)
+    graph.stubs(:get_picture).returns('http://foo.bar')
     Facebook.stubs(:connect).returns(graph)
   end
 
@@ -15,7 +47,7 @@ describe Facebook do
   end
 
   # Facebook::Page
-  describe Page do
+  describe Facebook::Page do
     it 'should have a build method' do
       Facebook::Page.must_respond_to :build
     end
@@ -26,7 +58,7 @@ describe Facebook do
       end
 
       it 'should raise an exception if a field is missing' do
-        ->{ Facebook::Page.build Struct.new(:fb_uid, :id, :name, :picture, :likes).new }.must_raise NoMethodError
+        ->{ Facebook::Page.build Struct.new(:fb_uid, :id, :name, :picture, :likes).new(fb_page['fb_uid']) }.must_raise NoMethodError
       end
 
       it 'should return the same object it received as an argument' do
@@ -39,6 +71,24 @@ describe Facebook do
         its { o.category.must_equal fb_page['category'] }
         its { o.picture.must_equal fb_page['picture'] }
         its { o.likes.must_equal fb_page['likes'] }
+      end
+    end
+  end
+
+  # Facebook::Feed
+  describe Facebook::Feed do
+    it 'should have a build method' do
+      Facebook::Feed.must_respond_to :build
+    end
+
+    describe '#build' do
+      let(:feed) { Facebook::Feed.build(Page.new(fb_uid: fb_page['fb_uid'])) }
+
+      it 'should have a list of comments' do
+        feed.comments.last.message.must_equal(fb_feed.last['message'])
+      end
+      it 'each comment should have an author' do
+        feed.comments.first.author.name.must_equal(fb_feed.first['from']['name'])
       end
     end
   end
