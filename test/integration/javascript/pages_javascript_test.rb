1844af2a49fe26bec83fe56944f82d52513b25fc
Gabriel Dehan <dehan.gabriel@gmail.com>
2012-12-30 21:11:29 +0100

    Javascript integration (backbone) tests

---
+++ test/integration/javascript/pages_javascript_test.rb
@@ -0,0 +1,37 @@
+require 'minitest_helper'
+
+describe 'Pages Javascript Integration Test' do
+  before do
+    visit root_path
+  end
+
+  context 'adding a new page' do
+    before do
+      @id = '218044128258628'
+    end
+
+    it 'must create a new page if the form is valid' do
+      within('#new_page') do
+        fill_in 'page_fb_uid', with: @id
+      end
+      click_button "add_page"
+      Page.last.fb_uid.must_equal @id
+    end
+
+    it 'must display an error if the form invalidates' do
+      within('#new_page') do
+        fill_in 'page_fb_uid', with: ''
+        click_button "add_page"
+      end
+      page.must have_css('div.alert-error')
+    end
+
+    it "must add the new page's name to the list", js: true do
+      within('#new_page') do
+        fill_in 'page_fb_uid', with: @id
+      end
+      click_button "add_page"
+      page.must have_css('li', Page.last.name)
+    end
+  end
+end
--- test/integration/routes_test.rb
+++ test/integration/routes_test.rb
@@ -18,4 +18,20 @@ describe 'Routes Acceptance Test' do
       assert_routing({ path: '/pages', method: :post }, { controller: 'pages', action: 'create' })
     end
   end
+
+  context 'api' do
+    describe '/api/pages' do
+     # The code here is much more complicated, as we already defined the route
+      # for the root_path and assert_routing only retrieves the first match
+      # between a route and a controller-action couple
+      before {  get('/api/pages') }
+
+      it 'should resolve' do
+        response.status.must_equal 200
+      end
+      it 'resolves to articles#index' do
+        request.parameters.must_equal({ "controller" => "pages", "action" => "index" })
+      end
+    end
+  end
 end
