c6aa7057904514074f329440a407510d58488c34
gabriel-dehan <dehan.gabriel@gmail.com>
2012-12-30 12:12:32 +0100

    Page helpers implementation

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,21 @@
+require 'time'
+
 module PagesHelper
+  def get_time time_string
+    time_ago_in_words(Time::parse(time_string))
+  end
+
+  def maybe_nil v
+    v.nil? ? 0 : v
+  end
+
+  def display_content_for comment
+    if comment.picture != nil
+      image   = link_to image_tag(comment.picture), comment.link
+      message = comment.message
+      return image + message
+    else
+      message = link_to comment.message, comment.link
+    end
+  end
 end
