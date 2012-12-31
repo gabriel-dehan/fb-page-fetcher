33befc53b62b704c033dd7ddb2fca14e26b3d2cd
Gabriel Dehan <dehan.gabriel@gmail.com>
2012-12-31 01:10:35 +0100

    Removing html no-js class when javascript is enabled

--- app/assets/javascripts/page_fetcher.js
+++ app/assets/javascripts/page_fetcher.js
@@ -4,6 +4,8 @@ window.PageFetcher = {
   Views: {},
   Routers: {},
   initialize: function() {
+      $('.no-js').removeClass('no-js');
+
       new PageFetcher.Routers.Pages
       Backbone.history.start({pushState:true})
   }
